e923218cc15547db797fabfe09a78f90e25de612
pndevops <144888081+pndevops@users.noreply.github.com>
2023-09-20 00:46:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3000; // Or your desired port
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(port, 'localhost', () => {
-  console.log(`Server is running on http://http://15.207.71.125/:${3000}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${port}`);
 });
