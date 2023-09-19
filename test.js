967e6ec1a0230681da087bd790432273ca46996d
pndevops <144888081+pndevops@users.noreply.github.com>
2023-09-19 20:37:36 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,21 +1,13 @@
-//const express = require('express');
-//const app = express();
-//const port = 3000; // Change this to your desired port
-
-//app.get('/', (req, res) => {
-//  res.send('welcome to Node JS project!');
-//});
-
-
 const express = require('express');
 const app = express();
+const port = 3000; // Replace with your desired port
 
-// Your Express application configuration and routes go here
+// Define a route for the root URL ("/")
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 
-const port = process.env.PORT || 3000; // You can change the port as needed
-
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
