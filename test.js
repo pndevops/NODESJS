48f61e43d43f657debd967233faf45752e8c0f39
pndevops <144888081+pndevops@users.noreply.github.com>
2023-09-19 20:20:35 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,11 @@
-const axios = require('axios');
-const { expect } = require('chai');
+const express = require('express');
+const app = express();
+const port = 3000; // Change this to your desired port
 
-describe('Application URL Test', () => {
-  it('should return a successful response', async () => {
-    const url = 'http://15.207.71.125:3000'; // Replace with your application URL
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 
-    try {
-      const response = await axios.get(url);
-      
-      // Add assertions here based on the response
-      expect(response.status).to.equal(200);
-      // You can add more assertions as needed
-      
-    } catch (error) {
-      throw new Error(`Failed to make a request to ${url}: ${error.message}`);
-    }
-  });
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
