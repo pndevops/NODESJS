fbe925125a53c9a300c41a9f44f22457a3ed28a1
pndevops <144888081+pndevops@users.noreply.github.com>
2023-09-20 00:59:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:${port}`);
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server is running on http://0.0.0.0:3000');
 });
+
